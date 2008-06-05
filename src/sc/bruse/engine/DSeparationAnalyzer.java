e250421cb4414e497617e98dcae357fb8317e31a
Scott Langevin <scott.langevin@gmail.com>
2008-06-05 19:22:11 -0400

    Added license info

--- src/sc/bruse/engine/DSeparationAnalyzer.java
+++ src/sc/bruse/engine/DSeparationAnalyzer.java
@@ -1,5 +1,29 @@
 package sc.bruse.engine;
 
+/***********************************
+ * Copyright 2008 Scott Langevin
+ * 
+ * All Rights Reserved.
+ *
+ * This file is part of BRUSE.
+ *
+ * BRUSE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * BRUSE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with BRUSE.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * @author Scott Langevin (scott.langevin@gmail.com)
+ *
+ */
+
 import java.util.*;
 
 import sc.bruse.network.*;
